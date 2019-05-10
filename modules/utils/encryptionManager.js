311b8036b7115bd1cd5cb34f09737094dafcc351
lumiere <heesung6701@naver.com>
2019-05-10 15:37:54 +0900

    separate a module for encryption

---
+++ modules/utils/encryptionManager.js
@@ -0,0 +1,24 @@
+const resMessage = require('./responseMessage')
+
+const crypto = require('crypto')
+
+const encryptionManager = {
+    encryption: (data, salt) => {
+        console.log(`salt: ${salt}`)
+        const hashedPwd = crypto.pbkdf2Sync(data, salt, 10000, 64, 'sha512').toString('base64')
+        return hashedPwd
+    },
+    makeRandomByte: () => {
+        return new Promise((resolve, reject) => {
+            crypto.randomBytes(32, function (err, buffer) {
+                if (err) {
+                    reject(err)
+                    return
+                }
+                resolve(buffer.toString('base64'))
+            })
+        })
+    },
+}
+
+module.exports = encryptionManager
