22b0a0c2d8697006e201f3a1235e99c981259935
lumiere <heesung6701@naver.com>
2019-05-10 15:38:29 +0900

    update board.js

--- routes/api/homework/board.js
+++ routes/api/homework/board.js
@@ -2,15 +2,18 @@ const express = require('express')
 const router = express.Router()
 
 const responseMessage = require('../../../modules/utils/responseMessage')
+const UTILS = require('../../../modules/utils/utils')
+const CODE = require('../../../modules/utils/statusCode')
+const csvManager = require('../../../modules/utils/csvManager')
+const encryptionManager = require('../../../modules/utils/encryptionManager')
 
-const url = require('url')
 const async = require('async')
 const crypto = require('crypto-promise')
 const csv = require('csvtojson')
 const json2csv = require('json2csv')
 const fs = require('fs')
 
-const csvFilePath = './public/csv/board_db.csv'
+const fileName = 'board_db.csv'
 
 /**
  * METHOD       : GET
@@ -20,13 +23,17 @@ const csvFilePath = './public/csv/board_db.csv'
 router.get('/:id', (req, res) => {
     const id = req.params.id
     console.log(`get method with id : ${id}`)
-    readCSV().then(jsonArr => {
+    if (id == undefined) {
+        res.status(200).send(UTILS.successFalse(CODE.BAD_REQUEST, responseMessage.OUT_OF_VALUE))
+        return
+    }
+    csvManager.csvRead(fileName).then(jsonArr => {
         jsonData = selectById(jsonArr, id)
         if (jsonData == false) {
-            writeRes(res, responseMessage.NO_BOARD)
+            res.status(200).send(UTILS.successFalse(CODE.NOT_FOUND, responseMessage.NO_BOARD))
             return
         }
-        writeRes(res, JSON.stringify(jsonData))
+        res.status(200).send(UTILS.successTrue(CODE.OK, jsonData))
     })
 })
 
@@ -36,89 +43,118 @@ router.get('/:id', (req, res) => {
  * PARAMETER    : (id = 게시물 고유 id), 
  *                  title = 게시물 제목, 
  *                  content = 게시물 내용, 
- *                  (write_time = 게시물 작성 시간),
  *                  password = 게시물 비밀 번호,
- *                  (salt = salt)
  */
 router.put('/', (req, res) => {
-    const json = req.body
-    console.log(`put method with ${JSON.stringify(json)}`)
+    console.log(`put method with ${JSON.stringify(req.body)}`)
 
-    async.waterfall([
-        // [task 1]  bring the database from csv file
-        function (callback) {
-            readCSV().then(jsonResult => callback(null, jsonResult))
-        },
-        // [task 2] encryption password
-        function (jsonArr, callback) {
-            json.salt = selectById(jsonArr, json.id).salt
-            encryption(json).then(jsonResult => callback(null, jsonArr, jsonResult))
+    const inputId = req.body.id
+    const inputTitle = req.body.title
+    const inputContent = req.body.content
+    const inputPwd = req.body.password
+
+    if (inputId == undefined ||
+        inputTitle == undefined ||
+        inputContent == undefined ||
+        inputPwd == undefined) {
+        res.status(200).send(UTILS.successFalse(CODE.BAD_REQUEST, responseMessage.OUT_OF_VALUE))
+        return
+    }
+
+    putBoard(inputId, inputTitle, inputContent, inputPwd).then((tf) => {
+        res.status(200).send(UTILS.successTrue(CODE.OK, responseMessage.UPDATED_BOARD))
+    }).catch((err) => {
+        console.log(err)
+        if (err.toString().length > 0)
+            res.status(200).send(UTILS.successFalse(CODE.BAD_REQUEST, err.toString()))
+        else {
+            res.status(200).send(UTILS.successFalse(CODE.INTERNAL_SERVER_ERROR, err.toString()))
         }
-    ], (err, jsonArr, jsonData) => {
-        if (err) { throw error('err') }
-
-        const state = update(jsonArr, jsonData)
-        console.log(state)
-        if (state != responseMessage.UPDATED_BOARD) {
-            writeRes(res, state)
-            return
-        }
-
-        writeCSV(csvFilePath, jsonArr).then(tf => {
-            if (!tf) {
-                console.log('tf : false')
-                return
-            }
-            writeRes(res, responseMessage.UPDATED_BOARD)
-        })
     })
 })
 
+async function putBoard(inputId, inputTitle, inputContent, inputPwd) {
+    const jsonArr = await csvManager.csvRead(fileName)
+
+    const jsonData = selectById(jsonArr, inputId)
+    if (jsonData == false) {
+        throw new Error(responseMessage.NO_BOARD)
+    }
+    const hashedPwd = await encryptionManager.encryption(inputPwd, jsonData.salt)
+
+    if (jsonData.password != hashedPwd) {
+        throw new Error(responseMessage.MISS_MATCH_PW)
+    }
+
+    jsonData.title = inputTitle
+    jsonData.content = inputContent
+
+    const tf = await csvManager.csvWrite(fileName, jsonArr)
+
+    if (!tf) {
+        throw new Error('')
+    }
+    return true
+}
 
 /**
  * METHOD       : DELETE
  * UTL          : /homework/board/
  * PARAMETER    : (id = 게시물 고유 id), 
- *                  title = 게시물 제목, 
- *                  content = 게시물 내용, 
- *                  (write_time = 게시물 작성 시간),
  *                  password = 게시물 비밀 번호,
- *                  (salt = salt)
  */
 router.delete('/', (req, res) => {
-    const json = req.body
-    console.log(`put method with ${JSON.stringify(json)}`)
+    console.log(`put method with ${JSON.stringify(req.body)}`)
 
-    async.waterfall([
-        // [task 1]  bring the database from csv file
-        function (callback) {
-            readCSV().then(jsonResult => callback(null, jsonResult))
-        },
-        // [task 2] encryption password
-        function (jsonArr, callback) {
-            json.salt = selectById(jsonArr, json.id).salt
-            encryption(json).then(jsonResult => callback(null, jsonArr, jsonResult))
-        }
-    ], (err, jsonArr, jsonData) => {
-        if (err) { throw error('err') }
-        
-        const state = remove(jsonArr, jsonData)
-        console.log(state)
-        if (state != responseMessage.REMOVED_BOARD) {
-            writeRes(res, state)
-            return
-        }
+    const inputId = req.body.id
+    const inputPwd = req.body.password
+    if (inputId == undefined || inputPwd == undefined) {
+        res.status(200).send(UTILS.successFalse(CODE.BAD_REQUEST, responseMessage.OUT_OF_VALUE))
+        return
+    }
 
-        writeCSV(csvFilePath, jsonArr).then(tf => {
-            if (!tf) {
-                console.log('tf : false')
-                return
-            }
-            writeRes(res, responseMessage.REMOVED_BOARD)
-        })
+    deleteBoard(inputId, inputPwd).then((tf) => {
+        if (tf == false) {
+            console.log('tf is false')
+            res.status(200).send(UTILS.successFalse(CODE.INTERNAL_SERVER_ERROR, REMOVED_BOARD_FAIL))
+        }
+        res.status(200).send(UTILS.successTrue(CODE.OK, responseMessage.REMOVED_BOARD))
+    }).catch((err) => {
+        console.log(err)
+        if (err.toString().length > 0)
+            res.status(200).send(UTILS.successFalse(CODE.BAD_REQUEST, err.toString()))
+        else {
+            res.status(200).send(UTILS.successFalse(CODE.INTERNAL_SERVER_ERROR, REMOVED_BOARD_FAIL))
+        }
     })
 })
 
+async function deleteBoard(id, pwd) {
+    const jsonArr = await csvManager.csvRead(fileName)
+
+    const jsonData = selectById(jsonArr, id)
+    console.log(jsonData)
+    if (jsonData == false) {
+        throw new Error(responseMessage.NO_BOARD)
+    }
+    const hashedPwd = await encryptionManager.encryption(pwd, jsonData.salt)
+
+    if (jsonData.password != hashedPwd) {
+        throw new Error(responseMessage.MISS_MATCH_PW)
+    }
+
+    const state = remove(jsonArr, id)
+    if (state != responseMessage.REMOVED_BOARD) {
+        throw new Error(state)
+    }
+
+    const tf = await csvManager.csvWrite(fileName, jsonArr)
+
+    if (!tf) {
+        throw new Error('')
+    }
+    return true
+}
 /**
  * METHOD       : POST
  * UTL          : /homework/board
@@ -130,28 +166,59 @@ router.delete('/', (req, res) => {
  *                  (salt = salt)
  */
 router.post('/', (req, res) => {
-    const json = req.body
-    console.log(`post method with ${JSON.stringify(json)}`)
+    console.log(`post method with ${JSON.stringify(req.body)}`)
+
+    const inputTitle = req.body.title
+    const inputContent = req.body.content
+    const inputPwd = req.body.password
+
+    if (inputTitle == undefined ||
+        inputContent == undefined ||
+        inputPwd == undefined) {
+        res.status(200).send(UTILS.successFalse(CODE.BAD_REQUEST, responseMessage.OUT_OF_VALUE))
+        return
+    }
+
+    const jsonInput = {
+        title: inputTitle,
+        content: inputContent,
+        password: inputPwd
+    }
 
     async.parallel([
         // [task 1]  bring the database from csv file
         function (callback) {
-            readCSV().then(jsonResult => callback(null, jsonResult))
+            csvManager.csvRead(fileName).then((jsonResult) => {
+                callback(null, jsonResult)
+            })
         },
         // [task 2] encryption password
         function (callback) {
-            encryption(json).then(json => callback(null, json))
+            const makeEncryption = async () => {
+                const salt = await encryptionManager.makeRandomByte()
+                const hashedPwd = await encryptionManager.encryption(jsonInput.password, salt)
+                const jsonData = {
+                    title: jsonInput.title,
+                    content: jsonInput.content,
+                    password: hashedPwd,
+                    salt: salt,
+                    date: new Date()
+                }
+                callback(null, jsonData)
+            }
+            makeEncryption()
         }
     ], (err, results) => {
         if (err) {
-            throw error('err')
+            res.status(200).send(UTILS.successFalse(CODE.BAD_REQUEST, err.toString()))
+            return
         }
 
         const jsonArr = results[0]
 
         //check the title is valid
-        if (checkDuplicate(jsonArr, json)) {
-            writeRes(res, responseMessage.ALREADY_BOARD)
+        if (checkDuplicateTitle(jsonArr, inputTitle)) {
+            res.status(200).send(UTILS.successFalse(CODE.BAD_REQUEST, responseMessage.ALREADY_BOARD))
             return
         }
 
@@ -165,12 +232,12 @@ router.post('/', (req, res) => {
         jsonArr.push(jsonData)
 
         //step 3. insert
-        writeCSV(csvFilePath, jsonArr).then(tf => {
+        csvManager.csvWrite(fileName, jsonArr).then(tf => {
             if (!tf) {
                 console.log('tf : false')
                 return
             }
-            writeRes(res, responseMessage.CREATED_BOARD)
+            res.status(200).send(UTILS.successTrue(CODE.OK, responseMessage.CREATED_BOARD))
             return
         })
     })
@@ -186,26 +253,10 @@ function selectById(jsonArr, id) {
     return false
 }
 
-function update(jsonArr, json) {
-    for (const idx in jsonArr) {
-        if (jsonArr[idx].id == json.id) {
-            if(jsonArr[idx].password != json.password){
-                return responseMessage.MISS_MATCH_PW
-            }
-            jsonArr[idx] = json
-            return responseMessage.UPDATED_BOARD
-        }
-    }
-    return responseMessage.NO_BOARD
-}
-
-function remove(jsonArr, json) {
+function remove(jsonArr, inputId) {
     let removeIdx = -1
     for (const idx in jsonArr) {
-        if (jsonArr[idx].id == json.id) {
-            if(jsonArr[idx].password != json.password){
-                return responseMessage.MISS_MATCH_PW
-            }
+        if (jsonArr[idx].id == inputId) {
             removeIdx = idx
             break
         }
@@ -217,77 +268,16 @@ function remove(jsonArr, json) {
     return responseMessage.REMOVED_BOARD
 }
 
-function checkDuplicate(jsonArr, newData) {
+function checkDuplicateTitle(jsonArr, inputTitle) {
     let isContains = false
     for (const idx in jsonArr) {
         const jsonData = jsonArr[idx]
-        if (jsonData.title == newData.title) {
+        if (jsonData.title == inputTitle) {
             isContains = true
             break
         }
     }
     return isContains
-}
-
-async function readCSV() {
-    const jsonArr = await readCsvFunction(csvFilePath)
-    return jsonArr
-}
-
-const readCsvFunction = (csvFilePath) => new Promise((resolve, reject) => {
-    csv().fromFile(csvFilePath).then((jsonArr) => {
-        console.log(`read success`)
-        resolve(jsonArr)
-    })
-})
-
-async function writeCSV(path, json) {
-    const fields = []
-    for (key in json[0]) {
-        fields.push(key)
-    }
-    const opts = {
-        fields
-    }
-    const parser = new json2csv.Parser(opts)
-    const resultCsv = parser.parse(json)
-
-    return new Promise(function (resolve, reject) {
-        fs.writeFile(path, resultCsv, (err) => {
-            if (err) {
-                reject(err)
-                return
-            }
-            resolve(true)
-        })
-    })
-}
-
-async function encryption(json) {
-    const data = json.password
-    console.log(`data: ${data}`)
-    
-    if(json.salt == undefined)
-        json.salt = (await crypto.randomBytes(32)).toString('base64')
-    console.log(`salt: ${json.salt}`)
-
-    let hashed = data
-    for (let i = 0; i < 1000; i++) {
-        hashed = await crypto.hmac('SHA512', json.salt)(hashed)
-    }
-    console.log(`hashed: ${hashed.toString('base64')}`)
-    json.password = hashed.toString('base64')
-
-    console.log(`result : ${JSON.stringify(json)}}`)
-    return json
-}
-
-function writeRes(res, msg) {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain; charset=UTF-8'
-    })
-    res.write(msg)
-    res.end()
 }
 
 module.exports = router
